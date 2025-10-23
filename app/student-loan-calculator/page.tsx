e96b093d37d4fd02ed9815541ddc008ff1a4a159
jnk1667 <122119276+jnk1667@users.noreply.github.com>
2025-10-23 11:39:37 +0530

    Update page.tsx

--- app/student-loan-calculator/page.tsx
+++ app/student-loan-calculator/page.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
     "private student loan calculator",
     "monthly payment calculator",
     "student debt calculator",
+    "best inflation calculator online",
     "loan interest calculator",
     "education loan calculator",
     "repayment plan comparison",
